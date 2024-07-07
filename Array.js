f30470c1ab37c24d6aefa6896eba66a22d219302
umarabid123 <umarabid709@gmail.com>
2024-07-07 21:19:44 +0500

    changes

--- Array.js
+++ Array.js
@@ -6,5 +6,108 @@ let num = [1,3,5,7];
 
  // Add Element at the last end of the array
 
- num.push(6);
- console.log(num);
+//  num.push(6);
+//  console.log(num);
+
+
+
+// Removing element at end of array 
+
+// num.pop()
+// console.log(num);
+
+
+// Fill all elemnts with same numbers
+
+// num.fill(2);
+// console.log(num);
+
+
+// Removing first element of the array
+
+// num.shift()
+// console.log(num);
+
+// Add Elemnts at the start of the array
+
+// num.unshift(9);
+// console.log(num);
+
+
+// Reverse array to accending or decending  order
+
+// num.reverse()
+// console.log(num);
+
+// num.reverse()
+// console.log(num);
+
+// Check elements is exist
+
+// console.log(num.includes(3));
+
+
+// map elements in the array
+
+// let num1 = num.map(item => 3*item)
+// console.log(num1);
+
+
+// filter the array  elements or etc
+
+// let num1 =num.filter(item => item > 5)
+// console.log(num1);
+
+
+// find element first less existing element in the array and if we used greater than 
+
+// console.log(num.find(item => item<5));
+
+
+// check if the every element of the array is greater then 0 or a specific number 
+
+// console.log(num.every(item => item <0));
+
+
+// find the index of the array 
+
+// if u have multiple same values the its return first existing position of this element
+
+// console.log(num.findIndex(item => item ==7));
+
+// convert integer to string
+
+// let num1 = num.toString();
+// console.log(typeof num1);
+
+
+// join array or etc in the arr 
+// let num2 = [9,10,12,13]
+// let num3 =[]
+//  num3 = num.join(num2)
+// console.log(num3);
+// console.log(num.join("*"));
+
+// Adding and removing Elements in enywhere in the array
+// num.splice(2, 0, 8,9 )
+// console.log(num);
+
+// slice element in the array and store in this slice in new array
+
+// console.log(num.slice(1,3));
+
+console.log(num.sort());
+
+// sort array elements in desending order 
+// console.log(num.sort(function(a,b){ return b-a} ));
+
+
+// sort array elements in Assending order 
+console.log(num.sort(function(a,b){ return a-b} ));
+
+
+
+
+
+
+
